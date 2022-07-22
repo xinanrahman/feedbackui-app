f6463a1baeeb2163060e743be86e5f539e0720b2
Xinan Rahman <xinanrahman03@gmail.com>
2022-07-22 18:57:52 -0400

    set up header component and global stylesheet

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+
+function Header(props) {
+  const headerStyles = {
+    backgroundColor: props.bgColor,
+    color: props.textColor,
+  };
+
+  return (
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{props.text}</h2>
+      </div>
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  text: "Feedback UI",
+  bgColor: "rgba(0, 0, 0, 0.4)",
+  textColor: "#ff6a95",
+};
+
+Header.propTypes = {
+  text: PropTypes.string,
+};
+
+export default Header;
